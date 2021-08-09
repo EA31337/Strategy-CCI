1974b1df8e08b4022a9d19c592db1a273c7e0ced
kenorb <kenorb@users.noreply.github.com>
2021-08-09 21:20:29 +0100

    Renames signal open filter param as per the latest classes change

--- config/M15.h
+++ config/M15.h
@@ -18,7 +18,6 @@ struct Stg_CCI_Params_M15 : StgParams {
   Stg_CCI_Params_M15() : StgParams(stg_cci_defaults) {
     lot_size = 0;
     signal_open_method = 2;
-    signal_open_filter = 32;
     signal_open_level = (float)20.0;
     signal_open_boost = 0;
     signal_close_method = 2;
